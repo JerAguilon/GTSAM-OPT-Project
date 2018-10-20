bbaa665dc84e466d2600e18b7b23977d8f34e66e
Jeremy Aguilon <jeraguilon@gmail.com>
2018-10-20 17:49:33 -0400

    Impl logger

---
+++ logger/logger.cc
@@ -0,0 +1,16 @@
+#include "logger/logger.h"
+
+std::unique_ptr<ExprAST> LogError(const char* Str) {
+    fprintf(stderr, "LogError: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char* Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+llvm::Value *LogErrorV(const char* Str) {
+    LogError(Str);
+    return nullptr;
+}
