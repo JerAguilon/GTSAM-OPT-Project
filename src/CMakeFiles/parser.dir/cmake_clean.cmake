f3d769aaabe1be647c637723882c71c72683fbe6
Jeremy Aguilon <jeraguilon@gmail.com>
2018-11-18 18:07:08 -0500

    Remove .gitignore'd files

--- src/CMakeFiles/parser.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "parser.pdb"
-  "parser"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/CMakeFiles/simple-parser.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libsimple-parser.a"
-)
